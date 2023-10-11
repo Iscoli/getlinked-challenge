6336c62813c6faf0e03812b12dab8fd13e298edc
Iscoli <96597856+Iscoli@users.noreply.github.com>
2023-10-11 01:59:15 -0700

    frame done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import SectionOne from '../component/SectionOne'
-import SectionTwo from '../component/SectionTwo'
-import SectionThree from '../component/SectionThree'
-import SectionFour from '../component/SectionFour'
-import SectionFive from '../component/SectionFive'
-import SectionSix from '../component/SectionSix'
-import SectionSeven from '../component/SectionSeven'
+import SectionOne from '../component/SectionOne';
+import SectionTwo from '../component/SectionTwo';
+import SectionThree from '../component/SectionThree';
+import SectionFour from '../component/SectionFour';
+import SectionFive from '../component/SectionFive';
+import SectionSix from '../component/SectionSix';
+import SectionSeven from '../component/SectionSeven';
+import SectionNine from '../component/SectionNine';
+import SectionTen from '../component/SectionTen';
+import Footer from '../component/Footer';
 
 
 
@@ -19,7 +21,9 @@ function Home() {
       <SectionFive/>
       <SectionSix/>
       <SectionSeven/>
-      
+      <SectionNine/>
+      <SectionTen/>
+      <Footer/>
     </>
   )
 }
